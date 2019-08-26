5cb682ea1636de4244981e7d95c75bada7026ec2
Andrey <Artecovec@users.noreply.github.com>
2019-08-26 12:43:35 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NotepadCSharp
+{
+    public partial class frmmain : Form
+    {
+        private int openDocuments = 0;
+        public frmmain()
+        {
+            InitializeComponent();
+            mnuSave.Enabled = false;
+        }
+
+        private void MnuNew_Click(object sender, EventArgs e)
+        {
+            //Создаем новый экземпляр формы frm
+            blank frm = new blank();
+            frm.DocName = "Untitled " + ++openDocuments;
+            frm.Text = frm.DocName;
+            //Указываем, что родительским контейнером
+            //нового экземпляра будет эта, главная форма.
+            frm.MdiParent = this;
+            //Вызываем форму
+            frm.Show();
+        }
+
+        private void MnuArrangeIcons_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.ArrangeIcons);
+        }
+
+        private void MnuCascade_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void MnuTileHorizontal_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void MnuTileVertical_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void MnuCut_Click(object sender, EventArgs e)
+        {
+            blank frm = (blank)this.ActiveMdiChild;
+            frm.Cut();
+        }
+
+        private void MnuCopy_Click(object sender, EventArgs e)
+        {
+            blank frm = (blank)this.ActiveMdiChild;
+            frm.Copy();
+        }
+
+        private void MnuPaste_Click(object sender, EventArgs e)
+        {
+            blank frm = (blank)this.ActiveMdiChild;
+            frm.Paste();
+        }
+
+        private void MnuDelete_Click(object sender, EventArgs e)
+        {
+            blank frm = (blank)this.ActiveMdiChild;
+            frm.Delete();
+        }
+
+        private void MnuSelectAll_Click(object sender, EventArgs e)
+        {
+            blank frm = (blank)this.ActiveMdiChild;
+            frm.SelectAll();
+        }
+
+        
+        
+
+        private void MnuOpen_Click_1(object sender, EventArgs e)
+        {
+            //Можно программно задавать доступные для обзора расширения файлов.
+            //openFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files(*.*)|*.*";
+            //Если выбран диалог открытия файла, выполняем условие
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Создаем новый документ
+                blank frm = new blank();
+                //Вызываем метод Open формы blank
+                frm.Open(openFileDialog1.FileName);
+                //Указываем, что родительской формой является форма frmmain
+                frm.MdiParent = this;
+                //Присваиваем переменной DocName имя открываемого файла
+                frm.DocName = openFileDialog1.FileName;
+                //Свойству Text формы присваиваем переменную DocName
+                frm.Text = frm.DocName;
+                //Вызываем форму frm
+                frm.Show();
+                mnuSave.Enabled = true;
+            }
+        }
+
+        private void MnuSave_Click(object sender, EventArgs e)
+        {
+            //Переключаем фокус на данную форму.
+            blank frm = (blank)this.ActiveMdiChild;
+            //Вызываем метод Save формы blank
+            frm.Save(frm.DocName);
+            frm.IsSaved = true;
+        }
+
+        private void MnuSaveAs_Click(object sender, EventArgs e)
+        {
+            mnuSave.Enabled = true;
+            //Можно программно задавать доступные для обзора расширения файлов
+            //openFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files(*.*)|*.*";
+            //Если выбран диалог открытия файла, выполняем условие
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Переключаем фокус на данную форму.
+                blank frm = (blank)this.ActiveMdiChild;
+                //Вызываем метод Save формы blank
+                frm.Save(saveFileDialog1.FileName);
+                //Указываем, что родительской формой является форма frmmain
+                frm.MdiParent = this;
+                //Присваиваем переменной FileName имя сохраняемого файла
+                frm.DocName = saveFileDialog1.FileName;
+                //Свойству Text формы присваиваем переменную DocName
+                frm.Text = frm.DocName;
+                frm.IsSaved = true;
+            }            
+        }
+
+        private void MnuFont_Click(object sender, EventArgs e)
+        {
+            //Переключаем фокус на данную форму.
+            blank frm = (blank)this.ActiveMdiChild;
+            //Указываем, что родительской формой является форма frmmain
+            frm.MdiParent = this;
+            //Вызываем диалог
+            fontDialog1.ShowColor = true;
+            //Связываем свойства SelectionFont и SelectionColor элемента RichTextBox
+            //с соответствующими свойствами диалога
+            fontDialog1.Font = frm.richTextBox1.SelectionFont;
+            fontDialog1.Color = frm.richTextBox1.SelectionColor;
+            //Если выбран диалог открытия файла, выполняем условие
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                frm.richTextBox1.SelectionFont = fontDialog1.Font;
+                frm.richTextBox1.SelectionColor = fontDialog1.Color;
+            }
+            //Показываем форму
+            frm.Show();
+        }
+
+        private void Frmmain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MnuColor_Click(object sender, EventArgs e)
+        {
+            blank frm = (blank)this.ActiveMdiChild;
+            frm.MdiParent = this;
+            colorDialog1.Color = frm.richTextBox1.SelectionColor;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                frm.richTextBox1.SelectionColor = colorDialog1.Color;
+            }
+            frm.Show();
+        }
+
+        private void MnuExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ blank.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace NotepadCSharp
+{
+    partial class blank
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cmnuCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmnuCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmnuPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmnuDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmnuSelectAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 450);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cmnuCut,
+            this.cmnuCopy,
+            this.cmnuPaste,
+            this.cmnuDelete,
+            this.cmnuSelectAll});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(132, 124);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.ContextMenuStrip1_Opening);
+            // 
+            // cmnuCut
+            // 
+            this.cmnuCut.Name = "cmnuCut";
+            this.cmnuCut.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.cmnuCut.ShowShortcutKeys = false;
+            this.cmnuCut.Size = new System.Drawing.Size(131, 24);
+            this.cmnuCut.Text = "&Cut";
+            this.cmnuCut.Click += new System.EventHandler(this.CmnuCut_Click);
+            // 
+            // cmnuCopy
+            // 
+            this.cmnuCopy.Name = "cmnuCopy";
+            this.cmnuCopy.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.cmnuCopy.ShowShortcutKeys = false;
+            this.cmnuCopy.Size = new System.Drawing.Size(131, 24);
+            this.cmnuCopy.Text = "&Copy";
+            this.cmnuCopy.Click += new System.EventHandler(this.CmnuCopy_Click);
+            // 
+            // cmnuPaste
+            // 
+            this.cmnuPaste.Name = "cmnuPaste";
+            this.cmnuPaste.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.cmnuPaste.ShowShortcutKeys = false;
+            this.cmnuPaste.Size = new System.Drawing.Size(131, 24);
+            this.cmnuPaste.Text = "&Paste";
+            this.cmnuPaste.Click += new System.EventHandler(this.CmnuPaste_Click);
+            // 
+            // cmnuDelete
+            // 
+            this.cmnuDelete.Name = "cmnuDelete";
+            this.cmnuDelete.ShortcutKeys = System.Windows.Forms.Keys.Delete;
+            this.cmnuDelete.ShowShortcutKeys = false;
+            this.cmnuDelete.Size = new System.Drawing.Size(131, 24);
+            this.cmnuDelete.Text = "&Delete";
+            this.cmnuDelete.Click += new System.EventHandler(this.CmnuDelete_Click);
+            // 
+            // cmnuSelectAll
+            // 
+            this.cmnuSelectAll.Name = "cmnuSelectAll";
+            this.cmnuSelectAll.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.cmnuSelectAll.ShowShortcutKeys = false;
+            this.cmnuSelectAll.Size = new System.Drawing.Size(131, 24);
+            this.cmnuSelectAll.Text = "&Select All";
+            this.cmnuSelectAll.Click += new System.EventHandler(this.CmnuSelectAll_Click);
+            // 
+            // blank
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "blank";
+            this.Text = "blank";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Blank_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Blank_FormClosed);
+            this.Load += new System.EventHandler(this.Blank_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cmnuCut;
+        private System.Windows.Forms.ToolStripMenuItem cmnuCopy;
+        private System.Windows.Forms.ToolStripMenuItem cmnuPaste;
+        private System.Windows.Forms.ToolStripMenuItem cmnuDelete;
+        private System.Windows.Forms.ToolStripMenuItem cmnuSelectAll;
+        public System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
